ce421dc2aad43daeca257e427cff522d0e40c5ba
JosephDuvvani <josephduvvani@gmail.com>
2024-10-11 19:28:58 +0200

    Create and display projects

--- src/index.js
+++ src/index.js
@@ -3,3 +3,33 @@ import Task from "./task";
 import Collections from "./collections";
 import './style.css';
 
+const collections = Collections();
+
+//Create New List
+const newProjectBtn = document.getElementById('add-project');
+
+newProjectBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const nameInput = document.getElementById('new-project-name');
+
+    const newProject = Project();
+    newProject.setName(nameInput.value);
+    collections.addProject(newProject);
+    
+    displayProjects();
+})
+
+//Display My Projects
+function displayProjects() {
+    const destination = document.getElementById('my-projects');
+    destination.textContent = '';
+    
+    const projects = collections.getMyProjects();
+
+    projects.forEach(proj => {
+        const item = document.createElement('li');
+        item.setAttribute('data-index', `${projects.indexOf(proj)}`);
+        item.textContent = proj.getName();
+        destination.appendChild(item);
+    })
+}
