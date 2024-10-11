e38e9dd577a00a617bc32acf0c0b2dc5cb1b3d67
JosephDuvvani <josephduvvani@gmail.com>
2024-10-11 20:01:55 +0200

    Add delete button for projects

--- src/index.js
+++ src/index.js
@@ -12,12 +12,28 @@ newProjectBtn.addEventListener('click', (e) => {
     e.preventDefault();
     const nameInput = document.getElementById('new-project-name');
 
+    if(nameInput.value === '') return;
     const newProject = Project();
-    newProject.setName(nameInput.value);
+    newProject.setName(nameInput.value.trim());
     collections.addProject(newProject);
     
     displayProjects();
 })
+
+//Delete Project
+function deleteProjectEventListener() {
+    const deleteProjectBtns = document.querySelectorAll('.delete-project');
+
+    deleteProjectBtns.forEach(btn => {
+        btn.addEventListener('click', (e) => {
+            const targ = e.target;
+            const index = targ.dataset.remove;
+            collections.removeProject(index);
+    
+            displayProjects();
+        })
+    })
+}
 
 //Display My Projects
 function displayProjects() {
@@ -29,7 +45,18 @@ function displayProjects() {
     projects.forEach(proj => {
         const item = document.createElement('li');
         item.setAttribute('data-index', `${projects.indexOf(proj)}`);
-        item.textContent = proj.getName();
+
+        const name = document.createElement('span');
+        name.textContent = proj.getName();
+        item.appendChild(name);
+
+        const deleteBtn = document.createElement('button');
+        deleteBtn.setAttribute('data-remove', `${projects.indexOf(proj)}`);
+        deleteBtn.classList.add('delete-project');
+        deleteBtn.textContent = '×';
+        item.appendChild(deleteBtn);
+
         destination.appendChild(item);
     })
+    deleteProjectEventListener();
 }
