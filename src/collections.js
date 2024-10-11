8f559c5b9a987c86af0b3d3e0459ad8bc92f810d
JosephDuvvani <josephduvvani@gmail.com>
2024-10-11 17:20:36 +0200

    Add projects array to collections
    -The array is to store all created projects

--- src/collections.js
+++ src/collections.js
@@ -1,6 +1,6 @@
 import Project from "./project";
 
-export default function TaskCollections() {
+export default function Collections() {
     const myDay = Project();
     myDay.setName('My Day');
 
@@ -10,13 +10,21 @@ export default function TaskCollections() {
     const allTasks = Project();
     allTasks.setName('All Tasks');
 
+    const myProjects = [];
+
     const getMyDay = () => myDay;
     const getImportant = () => important;
     const getAllTasks = () => allTasks;
+    const getMyProjects = () => myProjects;
+    const addProject = (proj) => myProjects.push(proj);
+    const removeProject = (index) => myProjects.splice(index, 1);
 
     return {
         getMyDay,
         getImportant,
-        getAllTasks
+        getAllTasks,
+        getMyProjects,
+        addProject,
+        removeProject
     };
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import Project from "./project";
 import Task from "./task";
-import TaskCollections from "./collections";
+import Collections from "./collections";
 import './style.css';
 
