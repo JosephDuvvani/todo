d8513f9a6361a2c0ec9a7d5ec9e7310e6fb859c4
JosephDuvvani <josephduvvani@gmail.com>
2024-10-12 15:07:31 +0200

    Display Tasks

--- src/index.js
+++ src/index.js
@@ -44,9 +44,11 @@ function displayProjects() {
 
     projects.forEach(proj => {
         const item = document.createElement('li');
+        item.classList.add('display-project');
         item.setAttribute('data-index', `${projects.indexOf(proj)}`);
 
         const name = document.createElement('span');
+        name.setAttribute('data-index', `${projects.indexOf(proj)}`);
         name.textContent = proj.getName();
         item.appendChild(name);
 
@@ -59,4 +61,82 @@ function displayProjects() {
         destination.appendChild(item);
     })
     deleteProjectEventListener();
+    projectOnClick();
+}
+
+//Display Tasks
+function displayTasks(selected) {
+    let proj;
+    if(selected === 'my-day') {
+        proj = collections.getMyDay();       
+    } else if(selected === 'important') {
+        proj = collections.getImportant();
+    } else if(selected === 'all-tasks') {
+        proj = collections.getAllTasks();
+    }  else if(+selected >= 0 || +selected <= collections.getMyProjects().length) {
+        proj = collections.getMyProjects()[selected];
+    }
+    
+    const title = document.getElementById('title');
+    const tasks = document.getElementById('tasks');
+
+    title.textContent = '';
+    tasks.textContent = '';
+
+    title.textContent = proj.getName();
+
+    const destination = document.getElementById('tasks');
+    const taskList = proj.getTasks();
+
+    taskList.forEach(task => {
+        const item = document.createElement('li');
+        item.classList.add('task-item');
+        item.setAttribute('data-index', `${taskList.indexOf(task)}`);
+
+        const name = document.createElement('span');
+        name.setAttribute('data-index', `${taskList.indexOf(task)}`);
+        name.textContent = task.getTitle();
+        item.appendChild(name);
+
+        const deleteBtn = document.createElement('button');
+        deleteBtn.setAttribute('data-removeTask', `${taskList.indexOf(task)}`);
+        deleteBtn.classList.add('delete-task');
+        deleteBtn.textContent = '×';
+        item.appendChild(deleteBtn);
+
+        destination.appendChild(item);
+    })
+}
+displayTasks('my-day');
+
+(function displayOnClick() {
+    const selected = document.querySelectorAll('.display-list');
+    const activeList = document.getElementById('tasks');
+
+    selected.forEach(proj => {
+        proj.addEventListener('click', (e) => {
+            const targ = e.target;
+            if(!targ.dataset.active) return;
+            const activeProject = targ.dataset.active;
+            activeList.dataset.current = activeProject;
+            
+            displayTasks(activeProject);
+        })
+    })
+})()
+
+function projectOnClick() {
+    const selected = document.querySelectorAll('.display-project');
+    const activeList = document.getElementById('tasks');
+
+    selected.forEach(proj => {
+        proj.addEventListener('click', (e) => {
+            const targ = e.target;
+            if(!targ.dataset.index) return;
+            const activeProject = targ.dataset.index;
+            activeList.dataset.current = activeProject;
+            
+            displayTasks(activeProject);
+        })
+    })
 }
